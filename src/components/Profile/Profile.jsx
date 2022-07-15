fccb9544ed6ad045112e81acdea9dac297782481
tylerjusfly <tylerjusfly1@gmail.com>
2022-07-15 17:18:45 +0100

    added react icons

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import './profile.css'
 import ProfileImg from '../../assets/images/profile.jpg'
+import { AiFillLinkedin, AiFillGithub ,AiFillTwitterCircle,} from 'react-icons/Ai'
 
 export const Profile = (props) => {
 
@@ -10,9 +11,9 @@ export const Profile = (props) => {
             <h1 className="fullname">{props[0].Fullname}</h1>
             <h2 className="desc">{props[0].occupation}</h2>   
             <ul className="social-list-inline">
-                <li><a href={props[0].github}>logo</a></li>                   
-                <li><a href={props[0].twitter}>logo</a></li>
-                <li><a href={props[0].linkedIn}>logo</a></li>                  
+                <li><a href={props[0].github} target='_blank'><AiFillGithub/> </a></li>                   
+                <li><a href={props[0].twitter} target='_blank'><AiFillTwitterCircle/> </a></li>
+                <li><a href={props[0].linkedIn} target='_blank'><AiFillLinkedin/> </a></li>                  
             </ul> 
         </div>
 </header>
